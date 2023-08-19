34b10a89d47b982d65f6b03c1f5cad34030de047
Lopatin E.A <i@lopatin-ea.ru>
2023-08-20 01:10:27 +0700

    add symbol size check

--- Wellcome_Kusr_end/Program.cs
+++ Wellcome_Kusr_end/Program.cs
@@ -1,17 +1,15 @@
 ﻿
 // Задача: Написать программу, которая
-//  из имеющегося массива строк формирует новый массив из строк,
-//   длина которых меньше, либо равна 3 символам. Первоначальный
-//    массив можно ввести с клавиатуры, либо задать на старте 
-//    выполнения алгоритма. При решении не рекомендуется пользоваться 
-//    коллекциями, лучше обойтись исключительно массивами.
+// из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. Первоначальный
+// массив можно ввести с клавиатуры, либо задать на старте 
+// выполнения алгоритма. При решении не рекомендуется пользоваться 
+// коллекциями, лучше обойтись исключительно массивами.
 
 // Примеры:
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
-
-using System.Globalization;
 
 Console.Write("Введите количество элементов массива: ");
 int length = Convert.ToInt32(Console.ReadLine());
@@ -26,22 +24,21 @@ for (int i = 0; i < length; i++)
 Console.WriteLine($"Массив: [{String.Join("; ", array)}]");
 
 
+
 int length2 = 0;
-string[] secondArray= new string[length2];
+
 for (int i = 0; i < length; i++)
+
 {
     if (array[i].Length <= 3)
     {
-        secondArray[j] = 0;
+        length2 ++;
     }
 }
-// string[] secondArray= new string[length2];
-// for (int i = 0; i < length; i++)
-// {
-//     if (array[i].Length <= 3)
-//     {
-//         length2 = 0;
-//     }
-// }
 
-Console.WriteLine($"Массив: [{String.Join("; ", secondArray)}]");
+Console.WriteLine(length2);
+
+// string[] secondArray = new string[length2];
+
+
+// Console.WriteLine($"Массив: [{String.Join("; ", secondArray)}]");
