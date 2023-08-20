d3ecea6c033444859a2ad929efa7033c8ff34afa
Lopatin E.A <i@lopatin-ea.ru>
2023-08-20 23:45:45 +0700

    add fill and print new array

--- Wellcome_Kusr_end/Program.cs
+++ Wellcome_Kusr_end/Program.cs
@@ -11,34 +11,45 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+// Вводим и печатаем массив
+
 Console.Write("Введите количество элементов массива: ");
 int length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
 
 string[] array = new string[length];
-
 for (int i = 0; i < length; i++)
 {
     Console.Write($"Введите элемент массива {i}: ");
     array[i] = Console.ReadLine();
 }
-Console.WriteLine($"Массив: [{String.Join("; ", array)}]");
+Console.WriteLine();
+Console.WriteLine($"Заданный массив: [{String.Join("; ", array)}]");
 
-
+// Считаем длинну нового массива
 
 int length2 = 0;
-
 for (int i = 0; i < length; i++)
-
 {
     if (array[i].Length <= 3)
     {
-        length2 ++;
+        length2++;
     }
 }
 
-Console.WriteLine(length2);
+// Запополням и печатем массив
 
-// string[] secondArray = new string[length2];
+string[] secondArray = new string[length2];
+int j = 0;
+for (int i = 0; i < length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        secondArray[j] = array[i];
+        j++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Массив: [{String.Join("; ", secondArray)}]");
+Console.WriteLine();
 
-
-// Console.WriteLine($"Массив: [{String.Join("; ", secondArray)}]");
